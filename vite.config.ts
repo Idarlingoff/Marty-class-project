d790e876d4140c1a643fadbf887e0d6b911adca1
bramandl <idarling.off@gmail.com>
2026-05-20 23:37:16 +0200

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Pour GitHub Pages : définir la base via variable d'environnement
-const base = process.env.GITHUB_PAGES === 'true' ? '/marty-class-project/' : '/'
-
 export default defineConfig({
   plugins: [react()],
-  base,
+  base: '/marty-class-project/',
   build: {
     rollupOptions: {
       output: {

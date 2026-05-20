061dc42161807372bcee8c79503792fbbc0749ca
bramandl <idarling.off@gmail.com>
2026-05-20 23:45:41 +0200

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/marty-class-project/',
+  base: isGitHubPages ? '/marty-class-project/' : '/',
   build: {
     rollupOptions: {
       output: {
